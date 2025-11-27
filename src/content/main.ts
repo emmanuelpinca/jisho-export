431e6dccd6262556b70571f3cb5f94cb1cd45de8
Emmanuel <emmanuel.pinca098@gmail.com>
2025-11-27 19:30:33 +1100

    feat: add button for jisho definition save

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,3 +1,21 @@
-console.log("Content script running on", window.location.href);
+import styles from "../global.css?inline";
 
-browser.runtime.sendMessage({ hello: "from content" });
+const style = document.createElement("style");
+style.textContent = styles;
+document.head.appendChild(style);
+
+const meaningCollection =
+  document.body.getElementsByClassName("meaning-wrapper");
+
+for (const meaning of meaningCollection) {
+  const container = document.createElement("div");
+  const button = document.createElement("button");
+
+  container.className = "w-full h-fit flex justify-end";
+  button.className =
+    "!text-link !text-xs !px-0 !pb-0 !pt-1 !bg-transparent opacity-30 hover:opacity-100 !underline";
+  button.textContent = "Save with Jisho Export ▸";
+
+  container.appendChild(button);
+  meaning.appendChild(container);
+}
