2a2fb99705f2c48015282bb1e99227afab483008
Emmanuel <emmanuel.pinca098@gmail.com>
2025-11-27 14:15:21 +1100

    build: added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
     rollupOptions: {
