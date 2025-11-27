08bf3ae9380c45f84aac39447f771e4a50a4b406
Emmanuel <emmanuel.pinca098@gmail.com>
2025-11-28 03:36:02 +1100

    feat: add stateful UI with front-backend interaction

--- src/content/main.ts
+++ src/content/main.ts
@@ -5,41 +5,95 @@ const style = document.createElement("style");
 style.textContent = styles;
 document.head.appendChild(style);
 
-const handleClick = (payload: SavePayloadType) => {
-  browser.runtime.sendMessage({
-    type: "save",
+const handleClick = (
+  type: string,
+  payload: SavePayloadType
+): Promise<boolean> => {
+  return browser.runtime.sendMessage({
+    type,
     payload,
   });
 };
 
-const conceptCollection = document.body.getElementsByClassName(
-  "concept_light clearfix"
-);
+const fetchData = async (payload: TitleType): Promise<StoredDataType> => {
+  const res = await browser.runtime.sendMessage({
+    type: "get",
+    payload,
+  });
+  return res;
+};
 
-for (const concept of conceptCollection) {
-  const title = findTitle(concept);
+const init = async () => {
+  try {
+    const conceptCollection = document.body.getElementsByClassName(
+      "concept_light clearfix"
+    );
 
-  const meaningCollection = concept.getElementsByClassName("meaning-wrapper");
-  for (const meaning of meaningCollection) {
-    const container = document.createElement("div");
-    const button = document.createElement("button");
+    for (const concept of conceptCollection) {
+      const title = findTitle(concept);
+      if (!title) continue;
+      const data = await fetchData(title);
 
-    container.className = "w-full h-fit flex justify-end";
-    button.className =
-      "!text-link !text-xs !px-0 !pb-0 !pt-1 !bg-transparent opacity-30 hover:opacity-100 !underline";
-    button.textContent = "Save with Jisho Export ▸";
+      const meaningCollection =
+        concept.getElementsByClassName("meaning-wrapper");
+      for (const meaning of meaningCollection) {
+        const container = document.createElement("div");
+        const button = document.createElement("button");
 
-    const definition = findDefinition(meaning);
+        const definition = findDefinition(meaning);
 
-    const payload: SavePayloadType = {
-      text: title?.text || "",
-      furigana: title?.furigana || "",
-      meaning: definition || "",
-    };
+        if (definition == undefined) {
+          continue;
+        }
 
-    button.onclick = () => handleClick(payload);
+        let saved = false;
 
-    container.appendChild(button);
-    meaning.appendChild(container);
+        if (data != undefined && data.meanings != undefined) {
+          saved = data.meanings.includes(definition || "");
+        }
+
+        container.className = "w-full h-fit flex justify-end";
+        button.className =
+          "!text-link !text-xs !px-0 !pb-0 !pt-1 !bg-transparent opacity-30 hover:opacity-100 !underline";
+
+        if (saved) {
+          button.textContent = "Unsave from Jisho Export ▸";
+        } else {
+          button.textContent = "Save with Jisho Export ▸";
+        }
+
+        const payload: SavePayloadType = {
+          text: title?.text || "",
+          furigana: title?.furigana || "",
+          meaning: definition || "",
+        };
+
+        button.onclick = async () => {
+          let res;
+
+          if (saved) {
+            res = await handleClick("unsave", payload);
+          } else {
+            res = await handleClick("save", payload);
+          }
+
+          if (res) {
+            saved = !saved;
+            if (saved) {
+              button.textContent = "Unsave from Jisho Export ▸";
+            } else {
+              button.textContent = "Save with Jisho Export ▸";
+            }
+          }
+        };
+
+        container.appendChild(button);
+        meaning.appendChild(container);
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
-}
+};
+
+init();
